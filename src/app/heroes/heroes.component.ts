7e1707e4aed8b6e109fc9bb2838c17221167497f
Kasra Naderi <naderi.kasra@gmail.com>
2021-01-22 17:31:28 -0500

    Turn the hero into a class 'hero.ts'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero: Hero = {
+    id: 1,
+    name: 'superman',
+  };
+  hero2: Hero = {
+    id: 2,
+    name: 'batman',
+  };
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
+
+
+
